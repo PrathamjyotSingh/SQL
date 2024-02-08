4b65c527652789b94bc598a1759a8a9d7cb6d8fb
Prathamjyot Singh <124479037+PrathamjyotSingh@users.noreply.github.com>
2024-02-08 21:22:20 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,16 @@
+USE CUSTOMERS
+--INSERTION
+--insert into work(e_id,e_name,e_rollno,e_country,gender) values  
+--(7,'srgs',3606,'INDIA','M')
+
+--UPDATE
+--update work set gender = 'F' where e_id = 7
+
+
+
+
+--delete from work where e_name = 'abc'
+
+
+
+SELECT * FROM work;
